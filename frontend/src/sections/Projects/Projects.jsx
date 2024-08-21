2c5d7092cf1c1de7482438e86d6fdd16adfdab1e
amul kumar <amul.kumar.min21@itbhu.ac.in>
2024-08-22 04:15:42 +0530

    project links working

--- frontend/src/sections/Projects/Projects.jsx
+++ frontend/src/sections/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/fresh-burger.png';
+import DDOS from '../../assets/DDOS.png';
+import EcommerceCart from '../../assets/EcommerceCart.png';
 import hipsster from '../../assets/hipsster.png';
 import fitLift from '../../assets/fitlift.png';
 import ProjectCard from '../../common/ProjectCard';
@@ -10,17 +10,17 @@ function Projects() {
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard
-          src={viberr}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Viberr"
-          p="Streaming App"
+        <ProjectCard 
+          src={DDOS}
+          link="https://ddos-stimulation.vercel.app/"
+          h3="DDOS"
+          p="React|Node|Mongo|Express"
         />
         <ProjectCard
-          src={freshBurger}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Fresh Burger"
-          p="Hamburger Restaurant"
+          src={EcommerceCart}
+          link="https://e-commerce-cart-theta.vercel.app/"
+          h3="E-commerce Cart"
+          p="React|Context Api"
         />
         <ProjectCard
           src={hipsster}
